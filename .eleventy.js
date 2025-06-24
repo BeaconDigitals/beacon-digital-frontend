71e67d187398d5c57c267347562bae684475188c
Beacon Digitals <beacondigitals6@gmail.comgit config --global user.email your@email.combeacondigitals6@gmail.com>
2025-06-24 06:48:01 -0700

    Show real blog posts from Eleventy/Netlify CMS on homepage

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("blog");
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      output: "_site"
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk"
+  };
+};
