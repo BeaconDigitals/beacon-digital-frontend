1d837dd8c4c11e9227172d42180f1551eb8051cf
unknown <beacondigitals6@gmail.com>
2026-02-26 06:01:21 -0800

    all Changes

--- .eleventy.js
+++ .eleventy.js
@@ -4,8 +4,13 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy("admin");
 
+    // Ignore .github directory to prevent processing of non-template files
+    eleventyConfig.ignores.add(".github/**");
+    // Ignore the subdirectory to prevent duplicate processing
+    eleventyConfig.ignores.add("beacon-digital-frontend-main/**");
+
     // Add blog collection if not already present
-    eleventyConfig.addCollection("blog", function(collectionApi) {      
+    eleventyConfig.addCollection("blog", function(collectionApi) {
         return collectionApi.getFilteredByGlob("blog/*.md");
     });
 
